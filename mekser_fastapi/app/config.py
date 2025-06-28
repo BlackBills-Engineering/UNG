93cbeb36ad28b66039b8669ccbc89fecf82ee931
mur073 <murodzon104@gmail.com>
2025-06-28 20:07:07 +0500

    meow

--- mekser_fastapi/app/config.py
+++ mekser_fastapi/app/config.py
@@ -16,12 +16,11 @@ TIMEOUT:    Final[float] = 0.5             # чтение 500 мс
 
 # -------- Pump addresses --------
 # Адрес = 0x50 + pump_id (1-based).
-DEFAULT_PUMP_IDS = list(range(0, 10))
+DEFAULT_PUMP_IDS = list(range(0, 62))
 
 # -------- Логика ----------------
 CRC_INIT = 0x0000
 CRC_POLY = 0x1021
-
 
 logger = logging.getLogger("mekser.config")
 logger.debug(f"Configuring SERIAL_PORT={SERIAL_PORT}, BAUDRATE={BAUDRATE}, TIMEOUT={TIMEOUT}")
